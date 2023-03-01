39e3d1a2321e8dae82de94dddd809d0d3ff25139
Karsten van der Deijl <k.c.vanderdeijl@student.tudelft.nl>
2023-03-01 14:27:35 +0100

    Removed unused imports from BoardController.java. To silence CI/CD pipeline

--- server/src/main/java/server/api/BoardController.java
+++ server/src/main/java/server/api/BoardController.java
@@ -21,14 +21,10 @@ import java.util.Random;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import commons.Quote;
 import server.database.BoardRepository;
-import server.database.QuoteRepository;
 
 import commons.*;
 

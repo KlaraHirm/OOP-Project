aecea08ef0e0692e23b409d3b3ce346d4af18a0f
Karsten van der Deijl <k.c.vanderdeijl@student.tudelft.nl>
2023-03-01 14:08:35 +0100

    Data Structure classes set up, and initial test backend api set up

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -1,5 +1,8 @@
 package commons;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,4 +29,21 @@ public class Board
        this.cardLists = new ArrayList<CardList>();
    }
 
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+
+        if (!(o instanceof Board)) return false;
+
+        Board board = (Board) o;
+
+        return new EqualsBuilder().append(id, board.id).append(title, board.title).append(cardLists, board.cardLists).isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37).append(id).append(title).append(cardLists).toHashCode();
+    }
 }
--- commons/src/main/java/commons/CardList.java
+++ commons/src/main/java/commons/CardList.java
@@ -1,5 +1,8 @@
 package commons;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,4 +29,23 @@ public class CardList
         this.title = title;
         this.cards = new ArrayList<Card>();
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+
+        if (!(o instanceof CardList)) return false;
+
+        CardList cardList = (CardList) o;
+
+        return new EqualsBuilder().append(id, cardList.id).append(title, cardList.title).append(cards, cardList.cards).isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37).append(id).append(title).append(cards).toHashCode();
+    }
+
 }

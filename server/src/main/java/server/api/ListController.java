34c3837fcdef5c493e50447bb7bc973e61f3bbae
Daniel <d.g.langov-1@student.tudelft.nl>
2023-03-10 20:26:43 +0100

    Add Edit/Delete request + refactor methods + documentation

--- server/src/main/java/server/api/ListController.java
+++ server/src/main/java/server/api/ListController.java
@@ -5,42 +5,94 @@ import commons.CardList;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.BoardRepository;
-
-import java.util.Optional;
+import server.database.CardListRepository;
 import java.util.Random;
 
 
 @RestController
 @RequestMapping("/api/list")
-public class ListController{
+public class ListController {
     private final Random random;
-    private final BoardRepository repo;
+    private final BoardRepository repoBoard;
+    private final CardListRepository repoList;
 
-    public ListController(Random random, BoardRepository repo) {
+    /**
+     * Constructor for the ListController
+     */
+    public ListController(Random random, BoardRepository repoBoard,
+                          CardListRepository repoList) {
         this.random = random;
-        this.repo = repo;
+        this.repoBoard = repoBoard;
+        this.repoList = repoList;
     }
 
-    @GetMapping(path = { "", "/" })
-    public ResponseEntity<CardList> getList(@RequestParam("list") long listId, @RequestParam("board") long boardId)
-    {
-        if (!repo.existsById(boardId)) return ResponseEntity.badRequest().build();
-        Board board = repo.findById(boardId).get();
-
-        Optional<CardList> list = board.cardLists.stream().filter(l -> l.id == listId).findFirst();
-
-        return ResponseEntity.ok(list.get());
+    /**
+     * Get info about a list
+     * @param listId - the id of the list
+     * @return - list object
+     * Returns 404 if the list does not exist
+     */
+    @GetMapping(path = {""})
+    public ResponseEntity<CardList> getList(@RequestParam("list") long listId) {
+        if (!repoList.existsById(listId)) return ResponseEntity.notFound().build();
+        CardList list = repoList.findById(listId).get();
+        return ResponseEntity.ok(list);
     }
 
-    @PostMapping(path = { "", "/" })
-    public ResponseEntity<Board> addList(@RequestBody CardList list, @RequestParam("board") long boardId)
-    {
+    /**
+     * Add a new list to a board
+     * @param boardId - the id of the board in which the list is
+     * @param list - the list object to add
+     * @return the whole board as updated
+     * Gives 404 if the board does not exist
+     * Gives 400 if the body is malformed
+     */
+    @PostMapping(path = {""})
+    public ResponseEntity<Board> addList(@RequestBody CardList list, @RequestParam("board") long boardId) {
         if (list == null) return ResponseEntity.badRequest().build();
-
-        if (!repo.existsById(boardId)) return ResponseEntity.badRequest().build();
-        Board board = repo.getById(boardId);
+        if (!repoBoard.existsById(boardId)) return ResponseEntity.notFound().build();
+        Board board = repoBoard.findById(boardId).get();
         board.cardLists.add(list);
-        Board saved = repo.save(board);
+        Board saved = repoBoard.save(board);
+        return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Update a certain list
+     * @param listTitle - the new title
+     * @param listId - the id of the list for update
+     * @return the edited list
+     * Gives 404 if the list does not exist
+     * Gives 400 if the title is malformed
+     */
+    @PutMapping(path = {""})
+    public ResponseEntity<CardList> editList(@RequestParam("title") String listTitle,
+                                             @RequestParam("list") long listId) {
+        if (listTitle == null) return ResponseEntity.badRequest().build();
+        if (!repoList.existsById(listId)) return ResponseEntity.notFound().build();
+        CardList list = repoList.findById(listId).get();
+        list.title = listTitle;
+        CardList saved = repoList.save(list);
+        return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Delete a list
+     * @param listId - the id of the list to delete
+     * @param boardId the id of the board in which the list is
+     * @return the whole board as updated
+     * Returns 404 if the list or board do not exist
+     */
+    @DeleteMapping(path = {""})
+    public ResponseEntity<Board> deleteList(@RequestParam("list") long listId,
+                                            @RequestParam("board") long boardId) {
+        if (!repoList.existsById(listId)) return ResponseEntity.notFound().build();
+        if (!repoBoard.existsById(boardId)) return ResponseEntity.notFound().build();
+        Board board = repoBoard.findById(boardId).get();
+        CardList list = repoList.findById(listId).get();
+        board.cardLists.remove(list);
+        Board saved = repoBoard.save(board);
+        repoList.deleteById(listId);
         return ResponseEntity.ok(saved);
     }
 }

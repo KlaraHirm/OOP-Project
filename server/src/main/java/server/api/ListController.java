bdb5155df1110054e81d5139820c5d03aa90b760
Klara Hirmanova <K.Hirmanova@student.tudelft.nl>
2023-04-07 13:35:45 +0200

    add bad request if parameters in reorder are invalid

--- server/src/main/java/server/api/ListController.java
+++ server/src/main/java/server/api/ListController.java
@@ -90,6 +90,9 @@ public class ListController {
     @PutMapping(path = {"/reorder"})
     public ResponseEntity<CardList> reorder(@RequestParam("original") long idOld, @RequestParam("target") long idNew,
                                   @RequestParam("cardId") long idCard, @RequestParam("cardPlace") int placeCard) {
+        if(placeCard < 1 || idOld < 0 || idNew < 0 || idCard < 0) {
+            return ResponseEntity.badRequest().build();
+        }
         CardList ret = listService.reorder(idOld, idNew, idCard, placeCard);
         if (ret == null) {
             return  ResponseEntity.notFound().build();

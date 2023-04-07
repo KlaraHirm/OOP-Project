64e98769d4f6f04674425423db2c386be68294e9
Alexander Nitters <a.f.nitters@student.tudelft.nl>
2023-04-07 19:10:44 +0200

    Apply suggestion call refresh after reorferCard

--- client/src/main/java/client/scenes/MainPageCtrl.java
+++ client/src/main/java/client/scenes/MainPageCtrl.java
@@ -361,6 +361,7 @@ public class MainPageCtrl implements Initializable {
      */
     public void reorderCard(Card card, CardList original, CardList target, int cardPlace) {
         server.editCardPosition(card, original, target, cardPlace);
+        refresh();
     }
 
     /**

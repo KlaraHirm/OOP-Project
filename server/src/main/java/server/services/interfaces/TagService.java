f568bcedb8216ac9eb269d40e5b0ba6d15a826c1
Daniel <d.g.langov-1@student.tudelft.nl>
2023-04-08 12:35:18 +0200

    Add TagService

--- server/src/main/java/server/services/interfaces/TagService.java
+++ server/src/main/java/server/services/interfaces/TagService.java
@@ -1,2 +1,19 @@
-package server.services.interfaces;public class TagService {
+package server.services.interfaces;
+
+import commons.Board;
+import commons.Tag;
+
+import java.util.List;
+
+public interface TagService {
+
+    List<Tag> getAllTags();
+
+    Tag getTag(long tagId);
+
+    Tag addTag(Tag tag);
+
+    Tag editTag(Tag tag);
+
+    Tag deleteTagWithId(long boardId);
 }

61fd0d1d7ace6758420a66f75d596b85bbaaa0f4
Klara Hirmanova <K.Hirmanova@student.tudelft.nl>
2023-04-13 21:12:05 +0200

    tag back and font color attributes

--- commons/src/test/java/commons/TagTest.java
+++ commons/src/test/java/commons/TagTest.java
@@ -63,18 +63,6 @@ class TagTest
     }
 
     /**
-     * Test that the Tag Equals method returns true and the hashcodes are equal even though the place is the same
-     */
-    @Test
-    public void testEqualsDifferentPlace()
-    {
-        Tag tag2 = new Tag("Test Tag");
-        tag2.id = 1L;
-        assertEquals(tag, tag2);
-        assertEquals(tag.hashCode(), tag2.hashCode());
-    }
-
-    /**
      * Test that the Tag Equals method returns false and the hashcodes differ when different Cards
      */
     @Test
@@ -86,6 +74,32 @@ class TagTest
         tag2.cards.add(card1);
         assertNotEquals(tag, tag2);
         assertNotEquals(tag2.hashCode(), tag.hashCode());
+    }
+
+    /**
+     * Test that the Tag Equals method returns false and the hashcodes differ when different backColor
+     */
+    @Test
+    public void testNotEqualsBackColor()
+    {
+        Tag tag2 = new Tag("Test Tag");
+        tag2.backColor = "#111111";
+        tag2.id = 1L;
+        assertNotEquals(tag, tag2);
+        assertNotEquals(tag.hashCode(), tag2.hashCode());
+    }
+
+    /**
+     * Test that the Tag Equals method returns false and the hashcodes differ when different fontCOlor
+     */
+    @Test
+    public void testNotEqualsFontCOlor()
+    {
+        Tag tag2 = new Tag("Test Tag");
+        tag2.fontColor = "#111111";
+        tag2.id = 1L;
+        assertNotEquals(tag, tag2);
+        assertNotEquals(tag.hashCode(), tag2.hashCode());
     }
 
     /**
